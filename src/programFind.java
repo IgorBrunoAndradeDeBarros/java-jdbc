9f7da1f3a84d6d1bb685ae9b767880883d6f3195
igord <igordf2502@gmail.com>
2026-07-13 00:50:54 -0300

    Cria program.java para fazer transação com jdbc

--- src/programFind.java
+++ src/programFind.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class program {
+public class programFind {
 
     public static void main(String[] args) {
 
